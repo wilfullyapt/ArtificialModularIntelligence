4eb281c5d08ed37378baf815b39d43ce9ac1a5ce
William Mawhinney <wilmawhinney@gmail.com>
2024-12-01 14:14:13 -0800

    google sync bugfix

--- ami/headspace/core/calendar/google_sync.py
+++ ami/headspace/core/calendar/google_sync.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from typing import Union, List
 
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import Resource, build
 from googleapiclient.errors import HttpError
 from google.oauth2.credentials import Credentials
